0606ff5ec048e0ce23219b0957e113ea65f5a454
Christian Höltje <docwhat@gerf.org>
2026-06-26 13:10:12 -0400

    add timeout cli flag for instrumentation

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"image/color"
 	"log"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
@@ -13,13 +15,19 @@ const (
 	screenHeight = 720
 )
 
-type Game struct{}
+type Game struct {
+	startTime   time.Time
+	quitAfterMs int
+}
 
 func (g *Game) Update() error {
-    if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
-       return ebiten.Termination
-    }
-    return nil
+	if g.quitAfterMs > 0 && time.Since(g.startTime).Milliseconds() >= int64(g.quitAfterMs) {
+		return ebiten.Termination
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		return ebiten.Termination
+	}
+	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
@@ -31,9 +39,17 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
+	quitAfterMs := flag.Int("quit-after-ms", 0, "Auto-quit after this many milliseconds (0 = never)")
+	flag.Parse()
+
+	game := &Game{
+		startTime:   time.Now(),
+		quitAfterMs: *quitAfterMs,
+	}
+
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Survive the Rail")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
